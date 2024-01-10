08f04a48528772139d1862743d8f190660d1c3e8
Bishoy Badie <Bishoy.Badei@linkdev.com>
2024-01-10 09:16:05 +0200

    Initial Commit

---
+++ LinkawyCloud/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LinkawyCloud
+//
+//  Created by Bishoy Badie on 10/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LinkawyCloud/LinkawyCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkawyCloudApp.swift
+//  LinkawyCloud
+//
+//  Created by Bishoy Badie on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LinkawyCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
